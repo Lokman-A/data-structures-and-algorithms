40b460dd088d5befac5ea470701a388f3b11014e
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-21 00:40:20 +0600

    time and space compx.

---
+++ 02-time-and-space-complexity.js
@@ -0,0 +1,3 @@
+// In computer science, the time complexity is the computational complexity that describes the amount of computer time it takes to run an algorithm.
+
+// The space complexity of an algorithm or a computer program is the amount of memory space required to solve an instance of the computational problem as a function of characteristics of the input. It is the memory required by an algorithm until it executes completely.
