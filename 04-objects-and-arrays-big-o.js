e7b8c88521a1ea4ee8c4f4fdc30e69ab866c39ac
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-21 20:05:28 +0600

    obj and arr

---
+++ 04-objects-and-arrays-big-o.js
@@ -0,0 +1,24 @@
+// objects
+const person = {
+  firstName: "Ayesha",
+  lastName: "Rahman",
+};
+
+// insert - O(1)
+// remove - O(1)
+// access - O(1)
+// search - O(n)
+// Object.keys() - O(n)
+// Object.values() - O(n)
+// Object.entries() - O(n)
+
+// arrays
+const even = [2, 4, 6, 8, 10];
+
+// insert or remove at the end - O(1)
+// insert or remove at the beginning - O(n)
+// access - O(1)
+// search - O(n)
+// push or pop - O(1)
+// shift, unshift, concat, slice or splice - O(n)
+// forEach, map, filter or reduce - O(n)
