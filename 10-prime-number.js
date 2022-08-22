13e1bd5994037cc1ef4cf015ca88432a02acc8fd
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-22 13:09:26 +0600

    prime

---
+++ 10-prime-number.js
@@ -0,0 +1,17 @@
+// give an natural number 'n', determine if the number is prime or not
+//isPrime(5) = true (1*5 or 5*1)
+//isPrime(4) = false (1*4 or 2*2 or 4*1)
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(45)); // O(n)
