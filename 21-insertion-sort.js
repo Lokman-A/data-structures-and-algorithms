f1297a81a32d4261f23a3fbc9d1e65806e7fcf94
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-25 21:15:34 +0600

    insertion sort

---
+++ 21-insertion-sort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+}
+
+const arr = [-6, 20, 8, -2, 4];
+insertionSort(arr);
+console.log(arr); // [-6, -2, 4, 8, 20]
+//O(n^2)
